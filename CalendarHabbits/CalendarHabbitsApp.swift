666456d7f3cfddb72999e16e4734176bf6907eed
PanAndrei <andrei_kmv@inbox.ru>
2022-08-17 17:43:17 +0300

    Initial Commit

---
+++ CalendarHabbits/CalendarHabbitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarHabbitsApp.swift
+//  CalendarHabbits
+//
+//  Created by Andrei Panasenko on 17.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalendarHabbitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarHabbits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalendarHabbits
+//
+//  Created by Andrei Panasenko on 17.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
